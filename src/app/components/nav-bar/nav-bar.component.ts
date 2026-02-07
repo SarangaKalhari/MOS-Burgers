39219c39d109997988d854fcc5edc684e13e2693
SarangaKalhari <kalharisaranga9@gmail.com>
2026-02-07 22:09:29 +0530

    Implement nav button component and add @Input to receive button data array

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+
+  @Input() button: any;
 
 }
