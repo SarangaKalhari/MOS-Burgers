54277ddfa02b1e1ff01356aac91588d2e500ccd5
SarangaKalhari <kalharisaranga9@gmail.com>
2026-02-25 00:02:30 +0530

    feat(order): implement order service for handling place order flow

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  private apiUrl = 'http://localhost:8080/order/place';
+
+  constructor(private http: HttpClient) {}
+
+  placeOrder(orderData: any): Observable<any> {
+    return this.http.post(this.apiUrl, orderData);
+  }
+
+  downloadInvoice(orderId: number): Observable<Blob> {
+    return this.http.get(
+      `${this.apiUrl}/invoice/${orderId}`,
+      { responseType: 'blob' }
+    );
+  }
+}
