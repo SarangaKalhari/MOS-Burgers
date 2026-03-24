ddbc4db36c26512cad800072c5691e1979fafdee
Saranga Kalhari <kalharisaranga9@gmail.com>
2026-03-24 12:24:10 +0530

    Added live search functionality on the search bar

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
+  searchText: string = '';
+
+  @Output() search = new EventEmitter<string>();
+
+  onSearch() {
+    this.search.emit(this.searchText);
+  }
+
+  onInput(){
+    this.search.emit(this.searchText); // 🔥 live emit
+  }
 
 }
+
+
