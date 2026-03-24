a2db108b98a9b84829b5b4e2838997f5cebd5a81
Saranga Kalhari <kalharisaranga9@gmail.com>
2026-03-24 12:35:26 +0530

    feat(search): implement live search across all items

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { BurgersComponent } from './pages/burgers/burgers.component';
 import { SalesComponent } from './pages/sales/sales.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: "sales",
         component: SalesComponent
+    },
+    {
+        path: "search",
+        component: SearchPageComponent
     }
 ];
