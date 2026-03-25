7ef031838819fede72dd03da14a09696f4fa55e1
Saranga Kalhari <kalharisaranga9@gmail.com>
2026-03-25 14:43:02 +0530

    Add search bar for item filtering

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from '../../components/side-bar/side-bar.component';
 import { ItemsComponent } from "../../components/items/items.component";
+import { SearchComponent } from "../../components/search/search.component";
 
 @Component({
   selector: 'app-orders',
-  imports: [SideBarComponent, ItemsComponent],
+  imports: [SideBarComponent, ItemsComponent, SearchComponent],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
